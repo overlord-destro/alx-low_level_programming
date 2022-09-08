4dbe89bf00aa9a2003f22f91db338d711641bb74
overlord-destro <edemcoffie41@gmail.com>
2022-09-08 22:23:52 +0000

    Added a script that compiles without warning

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Serves as an entry to the program
+ *
+ * return 0:this program should return 0 unless error
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
