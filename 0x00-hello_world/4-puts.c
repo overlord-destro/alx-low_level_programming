4bc4bbe4315c5070d431ead99c6dbb601832114d
overlord-destro <edemcoffie41@gmail.com>
2022-09-08 22:16:46 +0000

    Added a script that prints standard output using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point to the program
+ *
+ *Return:0 by default unless error
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
