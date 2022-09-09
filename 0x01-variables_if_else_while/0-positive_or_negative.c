6aecea785826b181dd0d8e9f1ea0878fe7e96a9a
overlord-destro <edemcoffie41@gmail.com>
2022-09-09 20:28:02 +0000

    Added a c file that prints out if a number is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - This serves as the entry point to the program
+ *
+ *Return:0 unless error
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0) {
+		printf("%d is positive\n", n);
+ 	}
+	else if (n == 0) {
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0) { 
+		printf("%d is negative\n", n);
+	}
+	return (0)
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This directory contains files on if else statements, while loops and for loops in C
