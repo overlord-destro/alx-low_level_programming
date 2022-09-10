73b9e3e1e640f4363b96b594d521ce5f111e0c71
overlord-destro <edemcoffie41@gmail.com>
2022-09-10 03:23:01 +0000

    Added a script that prints out alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - serves as the entry point to the program
+ *
+ *Return:0 unless error
+ *
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{	putchar (alph);
+		putchar (\n);
+	}
+	return (0);
+}
