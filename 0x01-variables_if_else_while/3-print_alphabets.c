f2e0d90529f784e9ce39c3e6fe10547a9d128b3a
overlord-destro <edemcoffie41@gmail.com>
2022-09-10 04:05:07 +0000

    Added a script that prints alphabets in lowercase then uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - serves as the entry point to the program
+ *
+ *Return:0 unless error
+ *
+ */
+int main(void)
+{
+	char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	int i = 0
+
+	for (; i < 52; i++)
+		putchar (alph[i]);
+	putchar('\n');
+	return (0);
+}
