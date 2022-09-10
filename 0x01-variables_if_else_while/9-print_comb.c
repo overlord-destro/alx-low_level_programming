9b8b84eba6cf8c91eff61add9c1aa03a9e2b3a93
overlord-destro <edemcoffie41@gmail.com>
2022-09-10 13:10:31 +0000

    Added a script that prints list of combination of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0;, i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar ((i % 10) + '0');
 		if (((i % 10) + '0') == '9')
 			break;
-
 		putchar (',');
 		putchar (' ');
 	}
