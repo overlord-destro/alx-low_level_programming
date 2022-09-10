ef5419b7363c8dcf78a015a02498aab7c2959cfe
overlord-destro <edemcoffie41@gmail.com>
2022-09-10 05:34:39 +0000

    Added a script that prints hexadimel numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - serves as the entry point to the program
+ *
+ *Return:0 unless error
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar (i);
+	for  (i = 97; i < 103; i++)
+		putchar (i);
+	putchar('\n');
+	return (0);
+}
