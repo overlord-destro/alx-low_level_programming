dd9bc7b1abc64d9f3cb4a86bc6f180be4af6a799
overlord-destro <edemcoffie41@gmail.com>
2022-09-13 22:49:34 +0000

    Added a program that prints from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_to_98 - prints for given natural number to 98
+ *
+ *@n: number to start from
+ *
+ *
+ *
+ */
+void print_to_98(int n)
+{
+	while (n < 99)
+	{
+		printf("%d",n);
+		_putchar(',');
+		_putchar(' ');
+	}
+	putchar('\n');
+}
