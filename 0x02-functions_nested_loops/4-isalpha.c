9aac29c37359074181537888a7b3dc5028216563
overlord-destro <edemcoffie41@gmail.com>
2022-09-13 21:16:24 +0000

    Added a function that checks if character is a letter at all

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isalpha - checks if a character is lowercase or uppercase
+ *
+ *@c: the character to be checked
+ *
+ *Return: 1 if lower or upper else returns 0
+ *
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
