1b86692a8dfeb3de9530282ca2b93c651056aa60
overlord-destro <edemcoffie41@gmail.com>
2022-09-13 19:51:27 +0000

    Added 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - prints _putchar followed by new line
+ *
+ *
+ *Return:0 
+ *
+ */
+int main(void)
+{
+	int charray[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(charray[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
+	
