e5eab4db0017149cc7378ea793c63de5cfe4481a
overlord-destro <edemcoffie41@gmail.com>
2022-09-13 19:59:37 +0000

    Added header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  *main - prints _putchar followed by new line
  *
@@ -13,9 +13,9 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-		putchar(charray[i]);
+		_putchar(charray[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
 
