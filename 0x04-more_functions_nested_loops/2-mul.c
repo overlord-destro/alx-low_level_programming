cbad97bee75d74406049b9506699f85c127514bf
overlord-destro <edemcoffie41@gmail.com>
2022-09-15 21:02:27 +0000

    Added a multiplication function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*mul - multiplies 2 integers
+*
+*@a: character to be multiplied
+*
+*@b: character to also be multiplied
+*
+*Return: 1 if succes 0 otherwise
+*
+*/
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
