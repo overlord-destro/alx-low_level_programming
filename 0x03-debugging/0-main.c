613db21174bb2f4c60a25b781f65ad0aa4d6f122
overlord-destro <edemcoffie41@gmail.com>
2022-09-15 20:06:31 +0000

    Added a file that tests a function with input 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*main - checks if number is positive or negative
+*
+*Return: 0 unless succes
+*
+*
+*/
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory contains files on debugging code in C
