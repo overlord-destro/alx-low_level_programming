e1a3e53264c1eda2a194afdf5b0c1b3300e6fd2f
overlord-destro <edemcoffie41@gmail.com>
2022-09-15 21:12:55 +0000

    Added a function that prints digits 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_numbers - prints digits 0 - 9
+*
+*
+*
+*
+*
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
