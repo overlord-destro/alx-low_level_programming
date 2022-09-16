5483ce0568b53582a4c6eeb25988f134d655d794
overlord-destro <edemcoffie41@gmail.com>
2022-09-16 21:10:01 +0000

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,16 +10,17 @@ int main(void)
 {
 	int i = 1;
 
-	for (i = 1; i < 101; i++)
+	printf("%d", i);
+	for (i = 2; i < 101; i++)
 	{
 		if ((i % 5) == 0 && (i % 3) == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf(" Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	putchar('\n');
 
