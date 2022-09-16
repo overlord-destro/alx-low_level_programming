634f80fa3456d3bd8174045f97e7cfeb45c45169
overlord-destro <edemcoffie41@gmail.com>
2022-09-16 20:05:11 +0000

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15, j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				_putchar((ch / 10) + 48);
-			_putchar((ch % 10) + 48);
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
