2e008a4969828bb7c8d8c13611c722fdfea5db07
overlord-destro <edemcoffie41@gmail.com>
2022-09-17 10:22:19 +0000

    updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+*main - prints out a quote by Dora korpar
+*
+*Return: 1
+*
+*
+*/
+int main(void)
+{
+	char charray[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, charray, 59);
+
+	return (1);
+}
