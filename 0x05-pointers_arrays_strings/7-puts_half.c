4a09527e1f38c7dad734de2568ef48f5ab8b18a5
overlord-destro <edemcoffie41@gmail.com>
2022-09-19 21:56:34 +0000

    Added a function that prints second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+*_strlen - find the length of a string
+*
+*@s: string to be checked
+*
+*Return: returns length of string
+*
+*
+*/
+int _strlen(char *s)
+{
+	int i = 0, count = 0;
+
+	while (*(s + i) != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
+/**
+*puts_half - prints the second half of a string
+*
+*@str: string to be halved
+*
+*/
+void puts_half(char *str)
+{
+	int i = 0, len;
+
+	len = _strlen(str);
+	if (len % 2 != 0)
+	{
+		len = (_strlen(str) - 1) / 2;
+		for (i = len / 2 ; i <= len, i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i <= len, i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
