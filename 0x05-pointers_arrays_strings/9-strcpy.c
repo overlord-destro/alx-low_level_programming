17301ca3aee6e081e485fcea64cf6b2a7497dc2b
overlord-destro <edemcoffie41@gmail.com>
2022-09-19 23:05:35 +0000

    Added a function that copies string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - copies the string pointed
+ * @dest: destination.
+ * @src: source.
+ * Return: pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
+			break;
+		i++;
+	}
+	return (dest);
+}
