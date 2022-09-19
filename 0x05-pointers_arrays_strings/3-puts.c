bfa30750b2e387a0345e14bd8357a9ac531df8f6
overlord-destro <edemcoffie41@gmail.com>
2022-09-19 20:14:03 +0000

    Added a function that prints out a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_puts - prints out a string followed by a new line
+*
+*@str: string to be printed
+*
+*
+*
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
