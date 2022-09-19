2595bf47e9e82efe5cac5a7489f0c66c2c33332b
overlord-destro <edemcoffie41@gmail.com>
2022-09-19 21:04:26 +0000

    Added a function that prints every other letter of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*puts2 - prints out every other character of a string then newline
+*
+*@str: string to be printed
+*
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+	_putchar('\n');
+}
