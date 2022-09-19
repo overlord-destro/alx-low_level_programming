ed8ff868cf82dafd9b646fd5896dec0bb2365b98
overlord-destro <edemcoffie41@gmail.com>
2022-09-19 19:50:51 +0000

    Added a function that swaps the positions of 2 integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*swap_int - swaps 2 variables positions
+*
+*@a: first variable to b swapped
+*
+*@b: second variable to be swapped
+*/
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	*a = c;
+	*a = *b;
+	*b = c;
+}
