c1615a4c42182bfd85e8a0e7165b6ae79c0ad854
overlord-destro <edemcoffie41@gmail.com>
2022-09-21 17:01:02 +0000

    Added a program that prints smallest possible combiantion of 2 digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - prints out smallest possible combination of 2 digit numbers
+*
+*Return: 0
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if (i >= j)
+			{
+				continue;
+			}
+			putchar(i);
+			putchar(j);
+			if (i == 56 && j == 57)
+				break;
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+	return (0);
+}
+
