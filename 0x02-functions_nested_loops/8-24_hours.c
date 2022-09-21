9e736556bd39c1327621df163251542026877d61
overlord-destro <edemcoffie41@gmail.com>
2022-09-21 09:31:45 +0000

    Added a function that counts from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*jack_bauer - prints out seconds of the day up to 23:59
+*
+*
+*/
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j < 52, j++)
+		{
+			for (k = 48; k < 54; k++)
+			{
+				for (l = 0; l <= 57; l++)
+				{
+					if (i == 50 && j == 51 && k == 53 && l == 57)
+						break;
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
