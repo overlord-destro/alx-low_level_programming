04a5cd50ccb041453f88cda10abebf46cbdf66a9
overlord-destro <edemcoffie41@gmail.com>
2022-09-26 20:52:10 +0000

    Added a function that fills up to n bytes with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,11 @@
 */
 char *_memset(char *s, char b, unisgned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
+	for (i = 0, i < n, i++)
 	{
 		*(s + i) = b;
-		i++;
 	}
-
 	return (s);
 }
