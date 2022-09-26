b244a561751b65142c89d11db7114fa011728b6e
overlord-destro <edemcoffie41@gmail.com>
2022-09-26 21:34:26 +0000

    Added a function that searches string for character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,10 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	while (s[i] != '\0')
-	{
+	for (; s[i] != '\0'; i++)
 		if (s[i] == c)
 			return (s + i);
-		else
-			return ('\0');
-		i++;
-	}
+	if (s[i] == c)
+		return (s + i);
+	return ('\0');
 }
