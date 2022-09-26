f150e63f8689f4bc289523a9c61e4b10e1ebd077
overlord-destro <edemcoffie41@gmail.com>
2022-09-26 21:13:51 +0000

    Added a function that searches string for character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_strchr - finds first occurence of character in string
+*@s: string to be searched
+*@c: character we are searching for
+*Return: pointer to c if found else null
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (c);
+		else
+			return (NULL);
+		i++;
+	}
+}
