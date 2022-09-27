0ec428f0b2a4f6054cb79f442cb42f811eb5ab7a
overlord-destro <edemcoffie41@gmail.com>
2022-09-27 05:08:41 +0000

    .

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_strpbrk - searches string for any bytes
+*@: string to be searched
+*Return: pointer to byte if found else null
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
