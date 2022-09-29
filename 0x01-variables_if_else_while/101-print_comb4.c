53e3c2d6ac99462eb98dd807695372aff45dee78
overlord-destro <edemcoffie41@gmail.com>
2022-09-29 22:25:32 +0000

    Added a function that prints out smallest possible combinations of 3 digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+*main - prints smallest possible combination of 3 digit numbers
+*
+*Return: always 0
+*
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				if (j >= k || i >= k || i >= k || i >= j)
+					continue;
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55 && j == 56 && k == 57)
+				break;
+				putchar(44);
+				putchar(' ');
+			}
+		}
+	}
+
+	return (0);
+}
