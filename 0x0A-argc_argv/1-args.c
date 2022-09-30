c25fe1e890ed91e3a72e75880acc56fd3288ef48
overlord-destro <edemcoffie41@gmail.com>
2022-09-30 21:37:00 +0000

    Added function that prints number of arguments in argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - prints out number of arguments in argv vector
+*
+*@argc: number of arguments in vector
+*@argv: argument vector
+*
+*Return: 0 unless error
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
