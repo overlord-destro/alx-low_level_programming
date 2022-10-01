0a47743515e9037d09456d087773ac0659031286
overlord-destro <edemcoffie41@gmail.com>
2022-10-01 11:26:25 +0000

    Added a function that find substring in string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*_strstr - finds first occurance of a substirng
+*@haystack: string to be searched
+*@needle: sub string we are searching for
+*Return: pointer to fisrt occurence of substring else null
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *aystack, *eendle;
+
+	while (*haystack)
+	{
+		aystack = haystack;
+		eedle = needle;
+
+		while (*haystack != '\0' && *eedle != '\0' && *haystack == *eedle)
+		{
+			haystack++;
+			eedle++;
+		}
+		if (!eedle)
+			return (aystack);
+		haystack = aystack + 1;
+	}
+	return ('\0')
+}
