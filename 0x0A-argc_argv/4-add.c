c399e2ea077053d4df8f3c051f04abe2161f52f9
overlord-destro <edemcoffie41@gmail.com>
2022-10-01 03:52:47 +0000

    Added an addition function

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+*main - adds 2 numbers
+*@argc: number of CL argumennts
+*@argv: argument array
+*Return: 0 unless error
+*
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, add;
+
+	add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	print("%d\n", add);
+	return (0);
+}
