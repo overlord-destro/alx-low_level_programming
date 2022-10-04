32b92afb8bbb270d4caffda8503f55f3967ac421
overlord-destro <edemcoffie41@gmail.com>
2022-10-04 20:58:08 +0000

    Added fucntion that concats 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	arr = (char *)malloc(sizeof(char) * (i + j + 1);
+	arr = (char *)malloc(sizeof(char) * (i + j + 1));
 
 	if (arr == NULL)
 	{
