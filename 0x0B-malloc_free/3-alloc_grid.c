a57f188d1612dc1d7735777d773092cefcfffba3
overlord-destro <edemcoffie41@gmail.com>
2022-10-04 22:39:55 +0000

    .

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (i--; i >= 0, i--)
+			for (i--; i >= 0; i--)
 				free(grid[i]);
 				return (NULL);
 		}
