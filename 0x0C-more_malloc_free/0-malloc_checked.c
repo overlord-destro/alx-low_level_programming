195ac29c845fe4ce1da29b255fe88d1855a92835
overlord-destro <edemcoffie41@gmail.com>
2022-10-06 20:16:44 +0000

    Added a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*malloc_checked - allocates memory using malloc
+*
+*@b: size to allocate
+*
+*Return: pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+	char *arr;
+
+	arr = malloc(sizeof(b) + 1);
+	if (arr == NULL)
+		exit(98);
+
+	return (arr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains more functions on using malloc to allocate memory dynamically
