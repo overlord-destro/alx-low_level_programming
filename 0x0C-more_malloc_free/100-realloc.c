bb2f0d7f36c2d8bfe9e02b4cfa7d5e388e258ffd
overlord-destro <edemcoffie41@gmail.com>
2022-10-08 15:13:56 +0000

    .

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,7 +20,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
 	free(ptr);
 
