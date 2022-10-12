04cd40bd1ce32b35b9e9ea464b50726d6143ae94
overlord-destro <edemcoffie41@gmail.com>
2022-10-12 03:56:23 -0700

    Added a function that executes a fucntion on an array of elements

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*array_iterator - executes one function on an array of elements
+*@array: array the function will be used on
+*@size: size of the array
+*@action: the function we are executing on the array
+*
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
