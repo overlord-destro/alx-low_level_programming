bd684b5ed01e123c6a8655225a5f1a26656f2369
overlord-destro <edemcoffie41@gmail.com>
2022-10-23 15:02:14 +0000

    .

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*list_len - returns number of elements in a linked list
+*@h: node on list
+*Return: number of elements on list
+*/
+size_t list_len(const list_h *h)
+{
+	size_t numbelem = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		numbelem++;
+	}
+
+	return (numbelem);
+}
