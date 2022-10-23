4e1f8b29a9624c58ad14d4a233d497790bebb3eb
overlord-destro <edemcoffie41@gmail.com>
2022-10-23 14:51:07 +0000

    .

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
 */
 size_t print_list(const list_t *h)
 {
-	size _t counter = 0;
+	size_t numbernodes = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[0] (nil)\n");
+
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		counter++;
+		numbernodes++;
 	}
 
-	return (counter);
+	return (numbernodes);
 }
