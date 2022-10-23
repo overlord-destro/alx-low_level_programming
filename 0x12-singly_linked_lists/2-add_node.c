7852731cfcbac9f29c6e2b2acb58af89645b98d2
overlord-destro <edemcoffie41@gmail.com>
2022-10-23 15:11:34 +0000

    .

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,4 +6,20 @@
 *Return: list
 */
 list_t *add_node(list_t **head, const char *str)
+{
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	list_t *newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->len = i;
+	newnode->str = strdup(str);
+
+	newnode->next = head;
+	*head = newnode;
+	return (*head);
+}
