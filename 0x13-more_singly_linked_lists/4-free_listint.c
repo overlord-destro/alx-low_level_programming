6e7032706575b5072b9ead3ef4933eab85e54b20
overlord-destro <edemcoffie41@gmail.com>
2022-10-24 04:20:10 -0700

    Added a function that frees a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+*free_listint - frees list
+*@head: head node of list
+*/
+void free_listint(listint_t *head)
+{
+	while (head != NULL)'
+	{
+		free(head);
+		head = head->next;
+	}
+}
