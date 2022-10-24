95a539cd0eec3f33ebd34b83644cb058ba5a8a2d
overlord-destro <edemcoffie41@gmail.com>
2022-10-24 07:30:55 -0700

    Added function that finds nth node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+*get_nodeint_at_index -returns nth node
+*@head: head node of list
+*@index: index of nth node
+*Return: nth node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *current;
+
+	current = head;
+
+	while (current != NULL && count != index)
+	{
+		count++;
+		current = current->next;
+	}
+
+	if (index > count)
+		return (NULL);
+
+	if (index == count)
+		return (current);
+}
