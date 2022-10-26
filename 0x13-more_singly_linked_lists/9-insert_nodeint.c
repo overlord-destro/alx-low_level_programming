1ed61bad058543fb9ca0eeac874a21f161decf0e
overlord-destro <edemcoffie41@gmail.com>
2022-10-26 06:50:39 -0700

    Added function that inserts node to nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	current = *head;
-	while (current != NULL && counter < idx)
+	while (current != NULL && counter < idx - 1)
 	{
 		current = current->next;
 		counter++;
