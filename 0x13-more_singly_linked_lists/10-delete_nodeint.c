873d7b65955f95ffbc6574b3d6084a6b4df0bc42
overlord-destro <edemcoffie41@gmail.com>
2022-10-28 07:26:49 -0700

    Added function that deletes node at given index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	current = *head;
-	if (current->next == NULL)
+	if (head->next == NULL)
 	{
 		free(current);
 		return (1);
