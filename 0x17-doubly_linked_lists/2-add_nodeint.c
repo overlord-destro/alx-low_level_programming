af380b650a8bc8f828669527c6082304d724a5bf
overlord-destro <edemcoffie41@gmail.com>
2022-12-08 14:39:55 +0000

    Added fucntion that adds node to beginning of list

--- 0x17-doubly_linked_lists/2-add_nodeint.c
+++ 0x17-doubly_linked_lists/2-add_nodeint.c
@@ -7,16 +7,20 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new, *temphead;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-	*head->prev = new;
-	new->next = *head;
 	new->prev = NULL;
+
+	temphead = *head;
+	new->next = temphead;
+	temphead->prev = new;
+
 	*head = new;
+
 	return (*head);
 }
