37cbb5fa03c8268070d580ae62f6017495def12a
overlord-destro <edemcoffie41@gmail.com>
2022-12-08 16:02:30 +0000

    tried to free int, fixed that

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,13 +5,9 @@
 */
 void free_dlistint(dlistint_t *head)
 {
-	if (*head)
+	while (head != NULL)
 	{
-		while (head != NULL)
-		{
-			free(head->n);
-			free(head);
-			head = head->next;
-		}
+		free(head);
+		head = head->next;
 	}
 }
