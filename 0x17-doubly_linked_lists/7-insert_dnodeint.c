d41fd69d240baa3c5e5fab60f51344a0fb906779
overlord-destro <edemcoffie41@gmail.com>
2022-12-08 17:14:59 +0000

    Added function that inserts node at given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		add_dnodeint(h, n);
+		newnode->next = NULL;
+		newnode->prev = NULL;
+		*h = newnode;
+		return (*h);
 	}
 	if (idx == 0 && h == NULL)
 	{
