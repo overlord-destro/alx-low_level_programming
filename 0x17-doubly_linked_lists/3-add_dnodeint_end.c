8dd9b65b840eb20ff78d3162b1d5b5c8e544bbe5
overlord-destro <edemcoffie41@gmail.com>
2022-12-08 15:42:22 +0000

    Added node to end of list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -3,6 +3,7 @@
 *add_dnodeint_end - adds node to end of list
 *@head: head node
 *@n: data
+*Return: returns address
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
@@ -10,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
